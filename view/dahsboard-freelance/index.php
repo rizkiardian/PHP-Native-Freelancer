72e746989172889d391a1c641ffadff482704e89
Dimas_Dr <dimasdliyaurrahman@gmail.com>
2023-11-09 23:25:09 +0700

    Menambahkan Navigasi Kategori pada Homepage

--- view/dahsboard-freelance/index.php
+++ view/dahsboard-freelance/index.php
@@ -5,11 +5,20 @@ require_once("../../controller/freelanceController.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nganggur | <?= $metadata["title"] ?></title>
-    <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Homepage | Nganggur</title>
+    <link rel="icon" type="image/x-icon" href="../../assets/logo/Logo.png" />
+    <!-- ICON -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <!-- Swiper -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <!-- CSS Ku -->
+    <link rel="stylesheet" href="../../css/style.css"/>
 </head>
 <body>
     
